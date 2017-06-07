10269897510a280b576cb8b5c2a93b12954d0c67
luannascimentobjj <nascimento.hate@gmail.com>
2017-06-07 18:28:50 -0300

    novas trs e form

--- js/principal.js
+++ js/principal.js
@@ -37,5 +37,38 @@ pacientes.forEach(function(paciente) {
 
 });
 
+var btAdd = document.querySelector("#adicionar-paciente");
 
+btAdd.addEventListener("click", function(event){
+	event.preventDefault();
 
+	var form = document.querySelector("#form-adiciona");
+
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+
+	var pacienteTr = document.createElement("tr");
+	var tabela = document.querySelector("#tabela-pacientes");
+
+	var nomeTd = document.createElement("td");
+	var pesoTd = document.createElement("td");
+	var alturaTd = document.createElement("td");
+	var gorduraTd = document.createElement("td");
+	var imcTd = document.createElement("td");
+
+	nomeTd.textContent = nome;
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura;
+
+	pacienteTr.appendChild(nomeTd);
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+
+	tabela.appendChild(pacienteTr);
+
+	console.log(pacienteTr);
+});
