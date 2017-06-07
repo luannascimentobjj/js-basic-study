8ca7873ad712c8d4670c712068584b811d63fa17
luannascimentobjj <nascimento.hate@gmail.com>
2017-06-07 16:10:07 -0300

    imc

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,35 @@
-		console.log("Foi carregado de um arquivo externo");
+var titulo = document.querySelector(".titulo"); 
+console.log(titulo.textContent);
 
-		var titulo = document.querySelector(".titulo"); 
-		console.log(titulo.textContent);
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+	console.log("Peso inválido");
+	pesoEhValido = false;
+	tdImc.textContent = "Peso Inválido";
+}
+
+if(altura <= 0 || altura >= 5.00){
+	console.log("Altura inválida");
+	alturaEhValida = false;
+	tdImc.textContent = "Altura Inválida";
+}
+
+if(alturaEhValida && pesoEhValido){
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc;
+}
+
+
